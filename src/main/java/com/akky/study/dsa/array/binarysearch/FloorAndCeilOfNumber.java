d63e4f1a7233297a094cb983490313baff41ebbd
Akbarabbas Shabbirali Kadaya <akbarabbas@Akbarabbass-MacBook-Air.local>
2023-08-25 07:02:29 +0530

    FloorCiel classname changed

--- src/main/java/com/akky/study/dsa/array/binarysearch/FloorAndCeilOfNumber.java
+++ src/main/java/com/akky/study/dsa/array/binarysearch/FloorAndCeilOfNumber.java
@@ -1,6 +1,6 @@
 package com.akky.study.dsa.array.binarysearch;
 
-public class FloorOrCeilOfNumber {
+public class FloorAndCeilOfNumber {
 	public static void main(String[] args) {
 		int[] arr = { -19, -13, -7, 0, 2, 4, 6, 9, 11, 12, 14, 20, 36, 48 };
 		int[] arr2 = { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130 };
